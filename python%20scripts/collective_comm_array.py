bff238dc1190311aa6516529deedeb0c7eae8986
Simen Mikkelsen <simen.mikkelsen@gmail.com>
2016-08-29 09:33:37 +0200

    Update collective_comm_array.py

--- python%20scripts/collective_comm_array.py
+++ python%20scripts/collective_comm_array.py
@@ -14,7 +14,7 @@ print("size:", size, "rank", rank, "data send : \n", sendbuf)
 recvbuf = None
 if rank == 0:
     recvbuf = np.zeros([size, 10, 10], dtype='i')
-comm.Gather(sendbuf+1, recvbuf, root=0)
+comm.Gather(sendbuf, recvbuf, root=0)
 if rank == 0:
     print("rank 0 data: \n", recvbuf)
     #for i in range(size):

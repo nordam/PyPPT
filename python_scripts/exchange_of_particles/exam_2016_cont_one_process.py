9633a36fabfaf9800a3ccb3c59348aee9348b7e7
Simen Mikkelsen <simen.mikkelsen@gmail.com>
2016-11-07 16:17:40 +0100

    new version via upload
    now with a trajectory of points

--- python_scripts/exchange_of_particles/exam_2016_cont_one_process.py
+++ python_scripts/exchange_of_particles/exam_2016_cont_one_process.py
@@ -16,17 +16,27 @@ mpi_size = comm.Get_size()
 
 ### INITIALIZING global parameters/properties
 
-# time and parameters of current veolicity field
-t = 0.0
+## time and parameters of current veolicity field
+    
+t_0 = 0.0
+t_plot = 2
 A = 0.1
-e = 0.25
-w = 1
+e = 0.25 # epsilon
+w = 1 # omega
 
-# transport parameters
-tmax = 4.0
-dt   = 0.1
+## transport parameters
+t_max = 20
+# point of interest
+XY_1   = [1.12, 0.6]
+XY_2   = [1.14, 0.6]
+XY_3   = [1.13, 0.58]
+XY_4   = [1.13, 0.62]
+# time step in Runge-Kutta integrator
+dt   = 0.01
+# multiplication factor to compare different time steps 
+dt_factor = 10
 
-# grid of points
+## grid of points
 # specify resolution
 Nx = 50
 Ny = 25
@@ -51,16 +61,13 @@ def doublegyre(x, y, t, A, e, w):
         ])
 
 # wrapper function to pass to integrator
-# X0 is a two-component vector [x, y]
+# XY is a two-component vector [x, y]
 def f(X, t):
-    # Parameters of the velocity field
-    A = 0.1
-    e = 0.25 # epsilon
-    w = 1    # omega
+    # Parameters of the velocity field is given at the start of this file
     return doublegyre(X[0], X[1], t, A, e, w)
 
 # 4th order Runge-Kutta integrator
-# X0 is a two-component vector [x, y]
+# XY is a two-component vector [x, y]
 def rk4(X, t, dt, f):
     k1 = f(X,           t)
     k2 = f(X + k1*dt/2, t + dt/2)
@@ -69,17 +76,17 @@ def rk4(X, t, dt, f):
     return X + dt*(k1 + 2*k2 + 2*k3 + k4) / 6
 	
 # function to calculate a trajectory from an
-# initial position X0 at t = 0, moving forward
-# until t = tmax, using the given timestep and
+# initial position XY at t = 0, moving forward
+# until t = t_max, using the given timestep and
 # integrator
-def trajectory(X0, tmax, dt, integrator, f):
-    t    = 0
+def trajectory(XY, t_max, dt, integrator, f):
+    t = t_0
     # Number of timesteps
-    Nx = int(tmax / dt)
+    Nx = int(t_max / dt)
     # Array to hold the entire trajectory
     PX = np.zeros((2, Nx+1))
     # Initial position
-    PX[:,0] = X0
+    PX[:,0] = XY
     # Loop over all timesteps
     for i in range(1, Nx+1):
         PX[:,i] = integrator(PX[:,i-1], t, dt, f)
@@ -98,40 +105,66 @@ y = np.linspace(y_min, y_max, Ny)
 X, Y = np.meshgrid(x, y)
 
 # Calculate field
-V = doublegyre(X, Y, t, A, e, w)
+V = doublegyre(X, Y, t_plot, A, e, w)
 
 # Vector plot
 fig = plt.figure(figsize = (16,8))
 plt.quiver(x, y, V[0], V[1], linewidths = -1.0, scale = 10, alpha = 0.6)
 # add text showing time, and set plot limits
-plt.text(1.65, 0.9, '$t = %s$' % t, size = 36)
+plt.text(1.65, 0.9, '$t = %s$' % t_plot, size = 36)
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
 plt.show()
 #plt.draw()
 
-### Plot trajectories
+### plot trajectories
 
 # Using Runge-Kutta
-
-X0   = [1.12, 0.6]
-tmax = 50
 
 # Set figure size
 fig = plt.figure(figsize = (12,6))
 
-# Plot trajectory for dt = 0.1
-dt   = 0.1
-T = trajectory(X0, tmax, dt, rk4, f)
-plt.plot(T[0,:], T[1,:], label = 'dt = %s' % dt)
+# Plot trajectory for dt (dt initialized at the start of this file)
 
-# Plot trajectory for dt = 0.01
-dt   = 0.01
-T = trajectory(X0, tmax, dt, rk4, f)
-plt.plot(T[0,:], T[1,:], '--', label = 'dt = %s' % dt)
+#T = trajectory(XY_1, t_max, dt, rk4, f)
+#plt.plot(T[0,:], T[1,:], label = 'dt = %s' % dt)
+
+# Plot trajectory for dt = dt*dt_factor
+#T = trajectory(XY, t_max, dt*dt_factor, rk4, f)
+#plt.plot(T[0,:], T[1,:], '--', label = 'dt = %s' % (dt*dt_factor))
+
+# Plot trajectory for 4 points:
+T = trajectory(XY_1, t_max, dt, rk4, f)
+plt.plot(T[0,:], T[1,:], label = 'x,y = %s' % XY_1)
+
+T = trajectory(XY_2, t_max, dt, rk4, f)
+plt.plot(T[0,:], T[1,:], label = 'x,y = %s' % XY_2)
+
+T = trajectory(XY_4, t_max, dt, rk4, f)
+plt.plot(T[0,:], T[1,:], label = 'x,y = %s' % XY_3)
+
+T = trajectory(XY_4, t_max, dt, rk4, f)
+plt.plot(T[0,:], T[1,:], label = 'x,y = %s' % XY_4)
 
 # Add legend and set limits
 plt.legend()
 plt.xlim(0,2)
 plt.ylim(0,1)
+plt.show()
+
+### plot grid of points before and after transport
+
+# Create lists of all x and y positions
+X  = np.linspace(x_min, x_max, Nx)
+Y  = np.linspace(y_min, y_max, Ny)
+
+# Array to hold all grid points
+XY = np.zeros((2, Ny, Nx))
+# Use meshgrid to turn lists into rank 2 arrays
+# of x and y positions
+XY[:] = np.meshgrid(X, Y)
+
+# Make scatter plot to show all grid points
+fig = plt.figure(figsize = (12,6))
+plt.scatter(XY[0,:], XY[1,:], marker = '.', c = '#348ABD')
 plt.show()

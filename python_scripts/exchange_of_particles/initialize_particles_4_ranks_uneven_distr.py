6f02769aa503876367ca1a5cc4a7c10d72e563f2
Simen Mikkelsen <simen.mikkelsen@gmail.com>
2016-11-14 20:13:02 +0100

    now also with y-positions

--- python_scripts/exchange_of_particles/initialize_particles_4_ranks_uneven_distr.py
+++ python_scripts/exchange_of_particles/initialize_particles_4_ranks_uneven_distr.py
@@ -16,7 +16,7 @@ import os # for IO-paths
 
 ## dividing of particles on ranks
 # total number of particles
-particle_n = 1000
+particle_n = 100
 distr = np.array([0*particle_n, .4*particle_n, .7*particle_n, .9*particle_n, 1*particle_n], dtype='int')
 # r0 = 400
 # r1 = 700
@@ -34,6 +34,7 @@ for rank in range(4):
     particle_local = np.arange(distr[rank], distr[rank+1])
 
     particle_x_local = np.random.rand(particle_n_local)
+    particle_y_local = np.random.rand(particle_n_local)
         
     # set all particles to active
     particle_active_local = np.ones(particle_n_local, dtype=bool)
@@ -41,13 +42,16 @@ for rank in range(4):
     # save files
     file_path_id        = os.path.join(IO_path, 'particle_id' + str(rank))
     file_path_x         = os.path.join(IO_path, 'particle_x' + str(rank))
+    file_path_y         = os.path.join(IO_path, 'particle_y' + str(rank))
     file_path_active    = os.path.join(IO_path, 'particle_active' + str(rank))
     
     np.save(file_path_id, particle_local)
     np.save(file_path_x, particle_x_local)
+    np.save(file_path_y, particle_x_local)
     np.save(file_path_active, particle_active_local)
     
     print(particle_n_local)
     print(particle_local)
     print(particle_x_local)
+    print(particle_y_local)
     print(particle_active_local)
